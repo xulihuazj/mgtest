0b6f50f433d3b0ce1ae84a0ba379e4434f680a55
xulihua <xulihua0124@gmail.com>
2018-10-08 17:45:18 +0800

    添加工具类
    添加排序 转换 Sort.Order 工具类

---
+++ mg-subtest/src/main/java/com/xulihuazj/mgsubtest/repository/SortEnum.java
@@ -0,0 +1,31 @@
+package com.xulihuazj.mgsubtest.repository;
+
+public enum SortEnum {
+
+    _ID_ASC("_ID", "ASC"),
+
+    _ID_DESC("_ID", "DESC"),
+
+    _NAME_ASC("_NAME", "ASC"),
+
+    _NAME_DESC("_NAME", "DESC"),
+    ;
+
+
+    String code;
+
+    String desc;
+
+    SortEnum(String code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
